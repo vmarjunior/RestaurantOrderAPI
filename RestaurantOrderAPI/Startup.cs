12b249e3f910a59547dab88a9bf37f05bf7c45bb
vmarjunior <valdir.mjunior@hotmail.com>
2020-11-29 15:23:54 -0300

    Removed Context

--- RestaurantOrderAPI/Startup.cs
+++ RestaurantOrderAPI/Startup.cs
@@ -27,11 +27,6 @@ namespace RestaurantOrderAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            /*services.AddDbContext<RestaurantOrderContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("Development"));
-            });
-            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
